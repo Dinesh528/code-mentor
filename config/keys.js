70bb7e9f51894de23a4c776ea7a2f6e9c93eac49
Dinesh <petadineshkumar97@gmail.com>
2020-03-29 16:52:36 +0530

    server with express router

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dinesh:dinesh123@cluster0-g6b5x.mongodb.net/test?retryWrites=true&w=majority'
+};
+
+// password: dinesh123@
+// username : dinesh
+// coomeetdinesh@gmail.com
+// mongodb atlas account 
