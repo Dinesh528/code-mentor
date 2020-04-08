b6e9b74e104374a22d08492c239c810021c9a9c4
Dinesh <petadineshkumar97@gmail.com>
2020-04-09 00:31:33 +0530

    dashboard update

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,10 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
+import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
 
 import './App.css';
 import Dashboard from './components/dashboard/Dashboard';
@@ -53,6 +56,18 @@ function App() {
 
             <Switch>
             <PrivateRoute exact path='/dashboard' component={Dashboard}/>
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path='/add-experience' component={AddExperience}/>
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path='/add-education' component={AddEducation}/>
             </Switch>
           </div>
         <Footer />
